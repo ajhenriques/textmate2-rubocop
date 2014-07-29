a775ecad520eb5cb8d1ea49f23785e18a7d20cb8
AJ Henriques <aj@10beat.com>
2014-07-28 21:23:34 -0700

    use expect syntax for specs

--- Support/spec/mate/env_spec.rb
+++ Support/spec/mate/env_spec.rb
@@ -7,25 +7,25 @@ class Dummy
 end
 
 describe 'mate env' do
-  
+
   it 'should define a project_path' do
-    Dummy.new.project_path.should =~ /Support/
+    expect(Dummy.new.project_path).to match /Support/
   end
-  
+
   it 'should define a vendor_path' do
-    Dummy.new.vendor_path.should =~ /Support\/vendor/
+    expect(Dummy.new.vendor_path).to match /Support/
   end
-  
+
   it 'should define a multiple_files' do
-    Dummy.new.multiple_files.should_not be_empty
+    expect(Dummy.new.multiple_files).to_not be_empty
   end
-  
+
   it 'should define single_file' do
-    Dummy.new.single_file.should =~ /Support/
+    expect(Dummy.new.single_file).to match /Support/
   end
-  
+
   describe 'names' do
-    
+
     ['John', 'John Smith', 'John Doe'].each do |val|
 
       before(:each) do
@@ -33,18 +33,18 @@ describe 'mate env' do
       end
 
       it 'should have user first name' do
-        Dummy.new.users_first_name.should =~ /jo/i
+        expect(Dummy.new.users_first_name).to match /jo/i
       end
     end
-    
+
     it 'should have an array of names' do
-      Dummy.new.users_names.length.should be(2)
+      expect(Dummy.new.users_names.length).to be(2)
     end
-    
+
     it "should not return anyting if the TM_FULLNAME is not set" do
       ENV['TM_FULLNAME'] = ''
-      Dummy.new.users_first_name.should == ''
+      expect(Dummy.new.users_first_name).to eq ''
     end
-  
+
   end
 end
